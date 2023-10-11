2b3666849533b03c768558695d0e14dcc91f5b80
JhulianDev <jhulian.dev@gmail.com>
2023-10-11 05:14:37 -0300

    Added color handler

---
+++ src/assets/colors/Colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  bgColor:          "#16093D",
+  lightBlue:        "#004FD4",
+  lightPurple:      "#9B47CF",
+  lightTurquoise:   "#00FFFF",
+  textColor:        "#ffffff"
+};
