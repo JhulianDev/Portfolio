bddbb06574c10343e33c9e4811b8eb11a8fc09a3
JhulianDev <jhulian.dev@gmail.com>
2023-10-11 05:23:47 -0300

    The Fonts.css file is imported

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router.jsx'
 import "./assets/css/Normalize.css"
+import "./assets/fonts/Fonts.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={Router} />
