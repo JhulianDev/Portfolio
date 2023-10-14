b014dda05c015774abe3b0d66b0d496cb8d19ad3
JhulianDev <jhulian.dev@gmail.com>
2023-10-14 06:32:54 -0300

    Section tag is removed

--- src/components/pages/error/ErrorStyles.jsx
+++ src/components/pages/error/ErrorStyles.jsx
@@ -1,15 +1,5 @@
 import styled from "styled-components"
 
-export const Section = styled.section`
-  background-color: #131313;
-  width: 100%;
-  height: calc(100vh - 60px);
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
 export const Title = styled.h1`
   color: white;
   font-family: Nunito;
