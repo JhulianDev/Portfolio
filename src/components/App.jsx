eccb5583757ac532911da21b8f560eb20e16bbc1
JhulianDev <jhulian.dev@gmail.com>
2023-10-18 06:04:21 -0300

    The Skills component is imported into the main application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './pages/home/Home';
 import Proyects from './pages/proyects/Proyects';
+import Skills from './pages/skills/Skills';
 
 const App = () => {
   return (
     <>
       <Home />
       <Proyects />
+      <Skills />
     </>
   );
 };
