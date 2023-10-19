efe75f7dcf2ab4c97fccbc3c8586281ac7f335c6
JhulianDev <jhulian.dev@gmail.com>
2023-10-19 05:59:15 -0300

    The AboutMe component and its styles are created

---
+++ src/components/pages/about/AboutMe.jsx
@@ -0,0 +1,29 @@
+import CardAboutMe from "../../general/cards/cardAboutMe/CardAboutMe";
+import { Light, MaxWidth, Section } from "../../general/styles/generalStyles";
+import ASTRONAUT_PORTRAIT from "../../../assets/img/AstronautPortrait.png";
+import { BoxCards, CardPortrait, TitleSection } from "./AboutMeStyles";
+
+const AboutMe = () => {
+  return (
+    <Section>
+      <Light $left $top $blue />
+      <Light $right $bottom $blue />
+      <TitleSection>About Me</TitleSection>
+      <MaxWidth>
+        <BoxCards>
+          <CardAboutMe
+            description="Hello! My name is Jhulian Mérida, a FullStack developer based in Argentina. I'm passionate about creating aesthetically pleasing and functional web interfaces that provide effective solutions. I'm constantly expanding my knowledge with new technologies and tools in order to enhance my technological stack and, in the near future, pass on my knowledge to new developers."
+          />
+
+          <CardPortrait src={ASTRONAUT_PORTRAIT} />
+
+          <CardAboutMe
+            description="Over the last 3 years, I've had the opportunity to work at a branding agency where my main role has been the comprehensive creation and management of brand identity for entrepreneurs. I'm responsible for designing logos, defining color palettes, selecting typefaces, and essentially bringing to life the visual identity that best represents each brand."
+          />
+        </BoxCards>
+      </MaxWidth>
+    </Section>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/pages/about/AboutMeStyles.jsx
@@ -0,0 +1,59 @@
+import styled from "styled-components"
+import { fonts } from "../../../assets/fonts/fontsHandler"
+
+export const TitleSection = styled.h1`
+  font-family: ${fonts.MainTypography};
+  color: white;
+  font-size: 3rem;
+  font-weight: 100;
+  text-align: center;
+  margin-bottom: 40px;
+  margin-top: -100px;
+
+  @media screen and (max-width: 1540px){
+    font-size: 2.3rem;
+    margin-bottom: 20px;
+    margin-top: -70px;
+  }
+
+  @media screen and (max-width: 820px){
+    font-size: 1.9rem;
+    margin-top: 0;
+  }
+`
+
+export const BoxCards = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  @media screen and (max-width: 820px){
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+  }
+`
+
+export const CardPortrait = styled.img`
+  width: 414px;
+
+  @media screen and (max-width: 1540px){
+    width: 300px;
+  }
+
+  @media screen and (max-width: 1180px){
+    width: 250px;
+  }
+
+  @media screen and (max-width: 900px){
+    width: 200px;
+  }
+
+  @media screen and (max-width: 820px){
+    width: 100%;
+    order: 1;
+    margin-bottom: -5px;
+  }
+`
