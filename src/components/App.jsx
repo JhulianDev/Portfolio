39d209a0764e151db812cf5139d1a5debaa3c043
JhulianDev <jhulian.dev@gmail.com>
2023-10-19 06:00:20 -0300

    The AboutMe component is imported into the main application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './pages/home/Home';
 import Proyects from './pages/proyects/Proyects';
 import Skills from './pages/skills/Skills';
+import AboutMe from './pages/about/AboutMe';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Home />
       <Proyects />
       <Skills />
+      <AboutMe />
     </>
   );
 };
