7dde4095055f18e88e58b483c3800517b2cd9e4f
JhulianDev <jhulian.dev@gmail.com>
2023-10-24 05:41:42 -0300

    The header component is imported and useRef is used to identify the rest of the application components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Home from './pages/home/Home';
 import Proyects from './pages/proyects/Proyects';
 import Skills from './pages/skills/Skills';
 import AboutMe from './pages/about/AboutMe';
 import Contact from './pages/contact/Contact';
+import Header from './general/header/Header';
 
 const App = () => {
+  const homeRef = useRef(null);
+  const proyectsRef = useRef(null);
+  const skillsRef = useRef(null);
+  const aboutMeRef = useRef(null);
+  const contactRef = useRef(null);
+
   return (
     <>
-      <Home />
-      <Proyects />
-      <Skills />
-      <AboutMe />
-      <Contact />
+      <Header 
+        homeRef={homeRef}
+        proyectsRef={proyectsRef}
+        skillsRef={skillsRef}
+        aboutMeRef={aboutMeRef}
+        contactRef={contactRef}
+      />
+      <Home sectionRef={homeRef} />
+      <Proyects sectionRef={proyectsRef} />
+      <Skills sectionRef={skillsRef} />
+      <AboutMe sectionRef={aboutMeRef} />
+      <Contact sectionRef={contactRef} />
     </>
   );
 };
