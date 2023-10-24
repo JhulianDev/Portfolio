d477b653c5a641c5bf0a8c60f29bbc3fa5ff7af9
JhulianDev <jhulian.dev@gmail.com>
2023-10-24 05:45:51 -0300

    Functionality is added to the component

--- src/components/general/header/Header.jsx
+++ src/components/general/header/Header.jsx
@@ -1,32 +1,50 @@
-import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import { ArrowIcon, DownloadButton, DownloadText, HeaderContainer, Item, ItemLink, List, Navbar } from "./HeaderStyles";
 import DOWNLOAD_ICON from "../../../assets/svg/Download-icon.svg"
+import HamburgerButton from '../buttons/hamburguer/HamburgerButton';
 
-const Header = () => {
-  const [itemActive, setItemActive] = useState("Home")
-  // const items = ['Home', 'Proyects', 'Skills', 'About me', 'Contact'];
-  const items = ['Home', 'Proyects', 'Skills'];
+const Header = ({ homeRef, proyectsRef, skillsRef, aboutMeRef, contactRef }) => {
+  const [sectionActive, setSectionActive] = useState("Home")
+  const [openNav, setOpenNav] = useState(false);
+  const itemsMenu = [
+    { name: 'Home', ref: homeRef },
+    { name: 'Proyects', ref: proyectsRef },
+    { name: 'Skills', ref: skillsRef },
+    { name: 'About me', ref: aboutMeRef },
+    { name: 'Contact', ref: contactRef },
+  ];
+  
+  const showNav = () => setOpenNav(!openNav);
+
+  const handleNav = (sectionRef, item) => {
+    sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    setSectionActive(item);
+    setOpenNav(false);
+  };
 
   return (
     <>
-      <HeaderContainer>
+      <HamburgerButton $open={openNav} showNav={showNav} />
+      <HeaderContainer $open={openNav}>
+
         <Navbar>
           <List>
-            {items.map((item) => (
-              <Item key={item}>
-                <ItemLink $active={item === itemActive} onClick={() => setItemActive(item)}>
-                  {item}
+            {itemsMenu.map((item) => (
+              <Item key={item.name}>
+                <ItemLink $active={item.name === sectionActive} onClick={() => handleNav(item.ref, item.name)}>
+                  {item.name}
                 </ItemLink>
               </Item>
             ))}
           </List>
         </Navbar>
-        
+
         <DownloadButton>
           <DownloadText>Curriculum</DownloadText>
           <ArrowIcon src={DOWNLOAD_ICON} />
         </DownloadButton>
+
       </HeaderContainer>
 
       <Outlet />
