834dbfd07e6d60a73fea2a4e0c57975ab8a8ad14
JhulianDev <jhulian.dev@gmail.com>
2023-10-26 07:27:03 -0300

    The use of refContext is implemented and the SwitchLenguage component is imported

--- src/components/general/header/Header.jsx
+++ src/components/general/header/Header.jsx
@@ -1,39 +1,36 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { RefContext } from '../../../context/RefContext';
 import { ArrowIcon, DownloadButton, DownloadText, HeaderContainer, Item, ItemLink, List, Navbar } from "./HeaderStyles";
 import DOWNLOAD_ICON from "../../../assets/svg/Download-icon.svg"
 import HamburgerButton from '../buttons/hamburguer/HamburgerButton';
+import SwitchLanguage from '../switch/SwitchLanguage';
 
-const Header = ({ homeRef, proyectsRef, skillsRef, aboutMeRef, contactRef }) => {
+const Header = () => {
+  const { refSections } = useContext(RefContext)
   const [sectionActive, setSectionActive] = useState("Home")
   const [openNav, setOpenNav] = useState(false);
-  const itemsMenu = [
-    { name: 'Home', ref: homeRef },
-    { name: 'Proyects', ref: proyectsRef },
-    { name: 'Skills', ref: skillsRef },
-    { name: 'About me', ref: aboutMeRef },
-    { name: 'Contact', ref: contactRef },
-  ];
   
   const showNav = () => setOpenNav(!openNav);
 
-  const handleNav = (sectionRef, item) => {
+  const handleNav = (sectionRef, id) => {
     sectionRef.current.scrollIntoView({ behavior: 'smooth' });
-    setSectionActive(item);
+    setSectionActive(id);
     setOpenNav(false);
   };
 
   return (
     <>
+      <SwitchLanguage showMobile={openNav} />
       <HamburgerButton $open={openNav} showNav={showNav} />
       <HeaderContainer $open={openNav}>
 
         <Navbar>
           <List>
-            {itemsMenu.map((item) => (
-              <Item key={item.name}>
-                <ItemLink $active={item.name === sectionActive} onClick={() => handleNav(item.ref, item.name)}>
-                  {item.name}
+            {refSections.map((section) => (
+              <Item key={section.name}>
+                <ItemLink $active={section.id === sectionActive} onClick={() => handleNav(section.ref, section.id)}>
+                  {section.name}
                 </ItemLink>
               </Item>
             ))}
