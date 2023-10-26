bb3d853915f34e40e060ca21bee9898a346ccbe6
JhulianDev <jhulian.dev@gmail.com>
2023-10-26 07:17:29 -0300

    The application is wrapped in the LanguageProvider and the RefProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,13 @@ import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router.jsx'
 import "./assets/css/Normalize.css"
 import "./assets/fonts/Fonts.css"
+import { LenguageProvider } from './context/LanguageContext.jsx'
+import { RefProvider } from './context/RefContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={Router} />
+  <LenguageProvider>
+    <RefProvider>
+      <RouterProvider router={Router} />
+    </RefProvider>
+  </LenguageProvider>
 )
