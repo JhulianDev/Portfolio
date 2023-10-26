d8e224bb65d5ac20caa0bf5d541002af255b1744
JhulianDev <jhulian.dev@gmail.com>
2023-10-26 07:25:13 -0300

    Constant refSections is created

--- src/context/RefContext.jsx
+++ src/context/RefContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useRef } from "react";
+import { createContext, useContext, useRef } from "react";
+import { LanguageContext } from "./LanguageContext";
 
 const RefContext = createContext();
 
@@ -9,7 +10,17 @@ const RefProvider = ({ children }) => {
   const aboutMeRef = useRef(null);
   const contactRef = useRef(null);
 
-  const data = { homeRef, proyectsRef, skillsRef, aboutMeRef, contactRef };
+  const { texts } = useContext(LanguageContext)
+  const t = texts.header
+  const refSections = [
+    { id: "Home", name: t.home, ref: homeRef },
+    { id: "Proyects", name: t.proyects, ref: proyectsRef },
+    { id: "Skills", name: t.skills, ref: skillsRef },
+    { id: "About Me", name: t.aboutMe, ref: aboutMeRef },
+    { id: "Contact", name: t.contact, ref: contactRef },
+  ];
+
+  const data = { homeRef, proyectsRef, skillsRef, aboutMeRef, contactRef, refSections };
 
   return (
     <RefContext.Provider value={data}>
