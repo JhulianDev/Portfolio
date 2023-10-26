b031f8b005819b6ebd7b820200c46db4be2ca41a
JhulianDev <jhulian.dev@gmail.com>
2023-10-26 07:08:05 -0300

    Ref handling is eliminated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Home from './pages/home/Home';
 import Proyects from './pages/proyects/Proyects';
 import Skills from './pages/skills/Skills';
@@ -7,26 +7,14 @@ import Contact from './pages/contact/Contact';
 import Header from './general/header/Header';
 
 const App = () => {
-  const homeRef = useRef(null);
-  const proyectsRef = useRef(null);
-  const skillsRef = useRef(null);
-  const aboutMeRef = useRef(null);
-  const contactRef = useRef(null);
-
   return (
     <>
-      <Header 
-        homeRef={homeRef}
-        proyectsRef={proyectsRef}
-        skillsRef={skillsRef}
-        aboutMeRef={aboutMeRef}
-        contactRef={contactRef}
-      />
-      <Home sectionRef={homeRef} />
-      <Proyects sectionRef={proyectsRef} />
-      <Skills sectionRef={skillsRef} />
-      <AboutMe sectionRef={aboutMeRef} />
-      <Contact sectionRef={contactRef} />
+      <Header />
+      <Home />
+      <Proyects />
+      <Skills />
+      <AboutMe />
+      <Contact />
     </>
   );
 };
