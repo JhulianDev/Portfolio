f1767a186f2687e9bfc7c069a5ffa5751ed530a1
JhulianDev <jhulian.dev@gmail.com>
2023-10-27 06:18:15 -0300

    Styles are removed

--- src/components/general/styles/generalStyles.jsx
+++ src/components/general/styles/generalStyles.jsx
@@ -19,7 +19,6 @@ export const Section = styled.section`
   }
 
   @media screen and (max-width: 820px){
-    padding: 60px 60px;
     min-height: auto;
   }
 `
@@ -38,45 +37,6 @@ export const MaxWidth = styled.div`
     gap: 40px;
   }
 `
-
-// export const Light = styled.span`
-//   width: 600px;
-//   height: 600px;
-//   border-radius: 1000px;
-//   filter: blur(200px);
-//   position: absolute;
-//   opacity: ${(props) => props.$opacity || "1"};
-//   z-index: -1;
-
-//   background-color: ${(props) => 
-//     props.$blue ? `${colors.lightBlue}` : 
-//     props.$purple ? `${colors.lightPurple}` : 
-//     props.$turquoise ? `${colors.lightTurquoise}` : "white"} 
-//   ;
-
-//   ${(props) => 
-//     props.$left ? css`
-//       left: -400px;
-//     ` : 
-//     props.$right ? css`
-//       right: -400px;
-//     ` : ''
-//   }
-
-//   ${(props) => 
-//     props.$top ? css`
-//       top: -400px;
-//     ` : 
-//     props.$bottom ? css`
-//       bottom: -400px;
-//     ` : ''
-//   }
-
-//   @media screen and (max-width: 820px){
-//     width: 480px;
-//     height: 480px;
-//   }
-// `
 
 export const Light = styled.span`
   aspect-ratio: 1/1;
