a004a4950cfd9381447b71e4f78418218f4ef812
JhulianDev <jhulian.dev@gmail.com>
2023-10-27 06:23:26 -0300

    Header element added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom"
 import Error from "../components/pages/error/Error"
 import App from "../components/App"
+import Header from "../components/general/header/Header"
 
 const Router = createBrowserRouter([
   {
     path: "/",
+    element: <Header />,
     errorElement: <Error />,
     children: [
       {
